af7899cc96fa944f6704c02fcf5e6269c4091ed6
dhyey0904 <rajadhyey1@gmail.com>
2026-08-21 17:41:57 +0530

    Initial commit

---
+++ DataType.py
@@ -0,0 +1,48 @@
+#numeric
+#int
+a=5
+print(a)
+print(type(a))
+
+# float
+b=5.5
+print(b)
+print(type(b))
+
+# complex
+c=5+2j
+print(c)
+print(type(c))
+
+# sequence types
+#list
+
+d=[1,2,3,4,5,6]
+print(d)
+print(type(d))
+print(d[2])
+d[2]=10
+print(d)
+
+#tuple
+e=(1,2,3,4,5,6)
+print(e)
+print(type(e))
+print(e[4])
+
+
+# string
+f="dhyey"
+print(f)
+print(f[2])
+
+# slicing
+g="mukhtar"
+print(g[0:7:2])
+
+# set
+mark_student={20,30,20,40,50}
+print(mark_student)
+mark_student1={20,30,40,50,20}
+print(mark_student1)
+print(mark_student==mark_student1)
