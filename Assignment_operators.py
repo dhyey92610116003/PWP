1c62f96ceed70d2ad1dbb6ac1febad9cca9a81b2
dhyey9610116003 <dhyey.raja153148@marwadiuniversity.ac.in>
2026-08-21 18:13:00 +0530

    Adding new files

---
+++ Assignment_operators.py
@@ -0,0 +1,15 @@
+n1=9
+n1+=3  # equivalent to n1 = n1 + 3
+print(n1)
+n1-=2  # equivalent to n1 = n1 - 2
+print(n1)
+n1*=4  # equivalent to n1 = n1 * 4
+print(n1)
+n1/=5  # equivalent to n1 = n1 / 5
+print(n1)
+n1//=2  # equivalent to n1 = n1 // 2
+print(n1)
+n1**=3  # equivalent to n1 = n1 ** 3
+print(n1)
+n1%=7  # equivalent to n1 = n1 % 7
+print(n1)
